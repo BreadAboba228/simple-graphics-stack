021d027274e83bdc7d1b195415f93a82615dec8b
BreadAboba228 <201282401+BreadAboba228@users.noreply.github.com>
2025-12-29 14:35:50 +0300

    examples instead of tests

--- src/engine.rs
+++ src/engine.rs
@@ -1,5 +1,6 @@
+use minifb::Window;
 use simple_linear_algebra_rs::{matrix::matrix4::Matrix4, vector::{quaternion::Quaternion, vec2::Vec2}};
-use simple_render_rs::render::{Render, app_handler::AppHandler, buffer::{Buffer, BufferSize}};
+use simple_render_rs::{color::Color, render::{Render, app_handler::AppHandler, buffer::{Buffer, BufferSize}}};
 
 use crate::engine::shape::{AngleUnit, Shape};
 
@@ -7,36 +8,43 @@ pub mod shape;
 
 pub struct Engine {
     shape: Shape,
-    ch: char,
+    color: Color,
     buffer_size: BufferSize,
     quater: Quaternion<f64>,
-    matrix: Matrix4<f64>
+    matrix: Matrix4<f64>,
+    vertexes_pool: Vec::<Vec2<isize>>
 }
 
 impl Engine {
     pub fn new(
         shape: Shape,
-        ch: char,
+        color: Color,
         buffer_size: BufferSize,
         angles: &[AngleUnit],
         matrix: Matrix4<f64>
     ) -> Self {
         let quater = AngleUnit::unification_to_quater(angles).to_normalized();
-        Self { shape, ch, buffer_size, quater, matrix }
+        let vertexes_pool = Vec::with_capacity(shape.vertexes().len());
+        Self { shape, color, buffer_size, quater, matrix, vertexes_pool }
     }
 
-    pub fn run(&mut self, fps: f64) {
-        let mut render = Render::new(self, fps);
+    pub fn run(&mut self, fps: f64, window: Window) {
+        let mut render = Render::new(self, fps, window);
 
         render.run();
     }
 
     pub fn to_real(&self, vec: Vec2<f64>) -> Vec2<isize> {
-        let x_ratio = self.buffer_size.height as f64 / self.buffer_size.width as f64;
+        //[-1; 1] + 1 -> [0; 2]
+        // [0; 2] / 2 -> [0; 1]
+        // [0; 1] * width -> [0; width]
+        let x = ((vec.x + 1.0) / 2.0) * self.buffer_size.width as f64;
 
-        let x = ((vec.x * x_ratio + 1.0) / 2.0) * self.buffer_size.width as f64;
-        // dividing y by 2 because of the character size
-        let y =  (1.0 - (vec.y / 2.0 + 1.0) / 2.0) * self.buffer_size.height as f64;
+        //[-1; 1] + 1 -> [0; 2]
+        // [0; 2] / 2 -> [0; 1]
+        // 1.0 - [0; 1] -> [1; 0] (axis reversal)
+        // [1; 0] * height -> [height; 0]
+        let y =  (1.0 - (vec.y + 1.0) / 2.0) * self.buffer_size.height as f64;
 
         Vec2::new(x as isize, y as isize)
     }
@@ -44,20 +52,27 @@ impl Engine {
 
 impl AppHandler for Engine {
     fn redraw(&mut self, buffer: &mut Buffer) {
+        let vertexes = self.shape.vertexes();
+
+        for i in vertexes {
+            let vertex3 = (self.matrix * (*i).to_homogeneous()).to_affine();
+            let vertex2 = self.to_real(vertex3.to_affine());
+            self.vertexes_pool.push(vertex2);
+        }
+
+        buffer.fill(Color::new(0));
 
         for edge in self.shape.edges() {
-            let point1 = (self.matrix * self.shape.vertexes()[edge.0].to_homogeneous()).to_affine();
-            let point2 = (self.matrix * self.shape.vertexes()[edge.1].to_homogeneous()).to_affine();
+            let start = self.vertexes_pool[edge.0];
 
-            let start = self.to_real(point1.to_affine());
-
-            let end = self.to_real(point2.to_affine());
+            let end = self.vertexes_pool[edge.1];
 
             //TODO: replace isize with usize in draw_line
-            buffer.draw_line(self.buffer_size, start, end, self.ch);
+            buffer.draw_line(self.buffer_size, start, end, self.color);
         }
 
         self.shape.raw_rotate(self.quater);
+        self.vertexes_pool.clear();
     }
 
 
